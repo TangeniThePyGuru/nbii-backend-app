1e32db1e9ab4c959b1bf6ea228d0d6641114e0cb
Albertus P Coetzee <coetzeealbertus@gmail.com>
2017-07-22 00:34:39 +0100

    added display routes and templets

--- routes/routes.js
+++ routes/routes.js
@@ -9,8 +9,20 @@ config('appRoutes', function($locationProvider, $routeProvider) {
     })
 
     .
+        when('/display-news', {
+            templateUrl: "views/news/display-news.htm",
+            controller: "newsController"
+    })
+
+    .
         when('/add-events', {
             templateUrl: "views/events/add-events.htm",
+            controller: "eventsController"
+    })
+
+    .
+        when('/display-events', {
+            templateUrl: "views/events/display-events.htm",
             controller: "eventsController"
     })
 
@@ -21,9 +33,20 @@ config('appRoutes', function($locationProvider, $routeProvider) {
     })
 
     .
+        when('/display-services', {
+            templateUrl: "views/services/display-services.htm",
+            controller: "servicesController"
+    })
+
+    .
         when('/add-adverts', {
             templateUrl: "views/adverts/add-adverts.htm",
             controller: "advertsController"
     })
 
+    .
+        when('/display-adverts', {
+            templateUrl: "views/adverts/dispaly-adverts.htm",
+            controller: "advertsController"
+    })
 });
