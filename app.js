6fbb385fdd39bdca5bfe3b8f9aa7323f5b77e6f4
Tangeni <tangenishikomba@gmail.com>
2017-08-01 17:38:40 +0100

    config removed from the index file and placed in the app.js file!

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 'use strict';
 // Declare app level module which depends on views, and components
-var app = angular.module('app', ["ngRoute","firebase" ,"appRoutes" , "appFactories", "appControllers", "ngAnimate"])
+var app = angular.module('app', ["ngRoute","firebase" ,"appRoutes" , "appFactories", "appControllers"])
     //app constants
     .constant('firebaseUrl',
         'https://nbiiapp.firebaseio.com/'
-    );
+    )
+    .config(function(){
+        var config = {
+            apiKey: "AIzaSyCCuyEMZMdMXgU3hGEPXaBHodQ5Gm6JaII",
+            authDomain: "nbiiapp.firebaseapp.com",
+            databaseURL: "https://nbiiapp.firebaseio.com",
+            projectId: "nbiiapp",
+            storageBucket: "nbiiapp.appspot.com",
+            messagingSenderId: "431754688791"
+        };
+        firebase.initializeApp(config);
+    });
 
 
